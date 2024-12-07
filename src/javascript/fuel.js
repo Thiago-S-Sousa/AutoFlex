5e4f3d71d0ba4224806eb2d1622577baaea608a5
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-12-06 21:55:34 -0300

    feat: fuel mode added, base code applied

--- src/javascript/fuel.js
+++ src/javascript/fuel.js
@@ -1,14 +1,42 @@
 const accordion = document.querySelector('.accordion');
-const accordionHeader = document.querySelector('.accordion-header');
 const accordionBody = document.querySelector('.accordion-body');
 
 accordion.addEventListener('click', function (event) {
 	event.preventDefault();
 
-	accordionHeader.classList.toggle('border-none');
 	accordionBody.classList.toggle('active');
-
-	console.log('Clicou!');
 });
 
-console.log(accordionHeader);
+const inputBtn = document.querySelector('#input-btn');
+const modal = document.querySelector('dialog');
+const closeModal = document.querySelector('#close-modal');
+
+inputBtn.addEventListener('click', function (event) {
+	event.preventDefault();
+
+	document.documentElement.style.setProperty('--margin', 'auto');
+	modal.showModal();
+});
+
+closeModal.addEventListener('click', function (event) {
+	event.preventDefault();
+
+	document.documentElement.style.setProperty('--margin', '--margin');
+	modal.close();
+});
+
+modal.addEventListener('keydown', function (event) {
+	switch (event.code) {
+		case 'F5':
+		case 'Escape':
+			event.preventDefault();
+			break;
+
+		case 'Backspace':
+			const target = event.target;
+			if (target.tagName !== 'INPUT' && target.tagName !== 'TEXTAREA') {
+				event.preventDefault();
+			}
+			break;
+	}
+});
