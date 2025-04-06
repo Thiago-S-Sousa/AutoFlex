b772c85f1cdaa43384e1627fe7e47ec3f4a06faa
Thiago Santos <Thiago_S_Sousa@outlook.com>
2025-04-05 21:36:18 -0300

    feat: add animations

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,4 +1,30 @@
+// ------ FOOTER ------
+
 const currentYear = new Date().getFullYear();
 const owner = "Thiago Santos";
 const text = `Copyright © ${currentYear}. Todos os direitos reservados a ${owner}.`;
 document.getElementById("copyright-text").textContent = text;
+
+// ------ ANIMATIONS ------
+
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const topElements = document.querySelectorAll(".top-hidden");
+const bottomElements = document.querySelectorAll(".bottom-hidden");
+const leftElements = document.querySelectorAll(".left-hidden");
+const rightElements = document.querySelectorAll(".right-hidden");
+
+topElements.forEach((element) => myObserver.observe(element));
+bottomElements.forEach((element) => myObserver.observe(element));
+leftElements.forEach((element) => myObserver.observe(element));
+rightElements.forEach((element) => myObserver.observe(element));
+
+// -----------------------------------------
